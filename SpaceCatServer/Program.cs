0aacaaa226c443e667a930f543171f46b76c3c16
danilkrotov <108077975+danilkrotov@users.noreply.github.com>
2022-12-06 18:22:45 +0500

    Add files via upload

---
+++ SpaceCatServer/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SpaceCatServer.Database;
+
+Console.WriteLine("Start init");
+DataBaseContext.AfterInit(); //database init
+new SpaceCatServer.Class.Discord(new Discord.WebSocket.DiscordSocketClient(), "");
+while (true);
